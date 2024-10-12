ecf3b47c2a44731bb5207c02690ec0a8dafa6058
Sounish Nath <flock.sinasini@gmail.com>
2024-10-13 01:25:57 +0530

    working on the migration init.sql postgres scripts

--- go-auth-service/internal/core/core.go
+++ go-auth-service/internal/core/core.go
@@ -1,6 +1,7 @@
 package core
 
 import (
+	"database/sql"
 	"log/slog"
 	"os"
 	"sync"
@@ -11,14 +12,24 @@ import (
 type Core struct {
 	PORT int
 	DSN  string
+	DB   *sql.DB
 	Lo   *slog.Logger
 	mu   sync.Mutex
 }
 
 func NewCore() *Core {
+	dsn := utils.GetEnv("DSN", "postgres://root:password@127.0.0.1:5432/auth?sslmode=disable").(string)
+	driver := utils.GetEnv("DRIVER", "postgres").(string)
+
+	db, err := sql.Open(driver, dsn)
+	if err != nil {
+		panic(err)
+	}
+
 	return &Core{
 		PORT: utils.GetEnv("PORT", 3000).(int),
-		DSN:  utils.GetEnv("DSN", "postgres://root:password@127.0.0.1:5432/auth?sslmode=disable").(string),
+		DSN:  dsn,
+		DB:   db,
 		Lo:   slog.New(slog.NewTextHandler(os.Stdout, nil)),
 	}
 }
