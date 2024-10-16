ab3fd1203d0aa5f6efbe34de4c67c0f57b281efe
Sounish Nath <flock.sinasini@gmail.com>
2024-10-16 17:07:30 +0530

    Update: Cloudsql proxy network on loopback added.

--- scripts/kubenetes_service_deployments.sh
+++ scripts/kubenetes_service_deployments.sh
@@ -7,6 +7,6 @@
 kubectl create deployment auth-service --image=asia-south1-docker.pkg.dev/sounish-cloud-workstation/fullstack-microservices-golang-gke/go-auth-service:v241000.3127 --port=3000 --replicas=2 --output=yaml --show-managed-fields=true --namespace=fullstack-microservice-golang --dry-run=client
 
 # -- Create a NodePort service for auth service 
-kubectl create service nodeport auth-service --tcp=3000 --output=yaml --namespace=fullstack-microservice-golang --dry-run=client
+kubectl create service clusterip auth-service --tcp=3000 --output=yaml --namespace=fullstack-microservice-golang --dry-run=client
 
 kubectl create deployment <deployment-name> --image=<image-name> --env="ENV_VAR1=value1" --env="ENV_VAR2=value2" --namespace=fullstack-microservice-golang --dry-run=client
